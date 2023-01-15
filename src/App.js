54c70389b86f053d7acf67bff3c458adc009921f
belaymit <2belamit@gmail.com>
2023-01-15 14:49:42 +0300

    added header and home components to App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './components/Cards';
+import CardDetails from './components/CardDetails';
+
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Cards />} />
+      <Route path="/cart" element={<CardDetails />} />
+    </Routes>
+  </>
+);
+
+export default App;
