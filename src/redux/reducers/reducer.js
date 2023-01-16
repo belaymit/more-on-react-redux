368ca8adcc597f0a9dc40e2deb5f994054e7131c
belaymit <2belamit@gmail.com>
2023-01-16 13:41:23 +0300

    defined Remove-Cart reducer

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,12 +1,20 @@
 const initialState = {
   carts: [],
 };
+
 const reducerFunctions = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
       return {
         ...state,
         carts: [...state.carts, action.payload],
+      };
+    case 'REMOVE_CART':
+      // eslint-disable-next-line no-case-declarations
+      const data = state.carts.filter((element) => element.id !== action.payload);
+      return {
+        ...state,
+        carts: data,
       };
     default:
       return state;
